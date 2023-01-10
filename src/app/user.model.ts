e255ef379b121989c34dc4d85147c08f9218d503
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-01-10 15:20:54 +0000

    BootCamp Week4 Day1 Daily Challenge

---
+++ src/app/user.model.ts
@@ -0,0 +1,15 @@
+export class User {
+
+    firstname: string;
+    lastname: string;
+    img: string;
+
+    constructor ( firstname: string, lastname:string, img:string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.img = img;
+      }
+
+}
+
+
